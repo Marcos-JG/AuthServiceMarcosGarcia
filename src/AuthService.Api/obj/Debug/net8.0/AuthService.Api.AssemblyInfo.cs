2c2e2893c607e4d5ef2db3091d49d9d80eb80db3
Marcos-JG <mgarcia-2024412@kinal.edu.gt>
2026-01-27 17:04:46 -0600

    Password completado

--- src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6c9627548d5f84863457005368661a80c5acd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead2e735331ae345ffded7e4fb019b7258d46242")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
